bd4681dfe6fb4f3b5056d3ee9b9fc3b2409468ad
Androz 2091 <androz2091@gmail.com>
2024-10-13 19:11:28 +0200

    add some more logs

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Creating constantes.py..."
+
 CONSTANTES_FILE="resources/constantes.py"
 CONSTANTES_CONTENT_PARSED=${CONSTANTES_CONTENT:-"please fill the CONSTANTES_CONTENT environment variable"}
 
